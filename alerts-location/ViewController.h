955c8f57dc025ad0539ce65580ac6f73e220ed76
Evangelos Giataganas <e.giataganas@gmail.com>
2016-03-24 13:08:39 +0200

    button changeAuth

--- alerts-location/ViewController.h
+++ alerts-location/ViewController.h
@@ -17,6 +17,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *status;
 @property(nonatomic)CLLocationManager *locationManager;
 @property(nonatomic)NSTimer *timer;
+- (IBAction)changAuth:(id)sender;
 
 @end
 
